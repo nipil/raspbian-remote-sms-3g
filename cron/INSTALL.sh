844b045fa6ddd715cedc81f5f474f5f7702fb5d9
nipil <nipil@users.noreply.github.com>
2019-12-29 21:23:52 +0100

    fix crontab loading when there is no crontab

--- cron/INSTALL.sh
+++ cron/INSTALL.sh
@@ -8,5 +8,4 @@ INSTALL_DIR=$(git rev-parse --show-toplevel)
 sudo rm -f ${TARGET}/${NAME} && \
 sed "s#INSTALL_SCRIPT_WILL_REPLACE_BY_GIT_REPO_BASE_DIR#${INSTALL_DIR}#" cron.sh | sudo tee ${TARGET}/${NAME} >/dev/null && \
 sudo chmod +x ${TARGET}/${NAME} && \
-crontab -r && \
 crontab crontab.txt
--- cron/README.txt
+++ cron/README.txt
@@ -1,8 +1,11 @@
 # config
 
+Ensure that you have a crontab, by using 'crontab -e' saving and exiting
+
 Add and update parameters in "cron.sh"
 
 Add or tune the entries you want in "crontab.txt"
+
 
 # install
 
